21c71bf5cf749c262a48f8c0c1eaf00f717886f6
BN0b13 <NobleNotes1@gmail.com>
2023-07-31 19:17:18 -0700

    shipping price adjustment for testing

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ export const deliveryInsurance = 800;
 export const shippingAndHandling = {
     'standard': {
         description: 'First Class 5-7 business days **USPS service subject to delays** Direct from USPS site: ALERT: As a result of the ongoing COVID-19 emergency, certain packages may take longer than usual to arrive.',
-        price: 699
+        price: 0
     }
 };
