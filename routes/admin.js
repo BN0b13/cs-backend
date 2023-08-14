cee13787c5567ca183dec31d9dbb3214e2f0479b
BN0b13 <NobleNotes1@gmail.com>
2023-08-14 12:30:52 -0700

    removing admin customer create logic

--- routes/admin.js
+++ routes/admin.js
@@ -106,7 +106,6 @@ router.get('/admin', AdminTokenVerifier, HandleErrors(userController.getAdmin));
 router.post('/employees', AdminTokenVerifier, HandleErrors(userController.createEmployee));
 router.get('/employees', AdminTokenVerifier, HandleErrors(userController.getEmployees));
 
-router.post('/customers', AdminTokenVerifier, HandleErrors(userController.adminCreateCustomer));
 router.get('/customers', AdminTokenVerifier, HandleErrors(userController.getCustomers));
 
 router.get('/users', AdminTokenVerifier, HandleErrors(userController.getUsers));
